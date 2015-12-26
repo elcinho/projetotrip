124ec137109c9e8d917d17e7cccf184ee8e1743e
elcinho <elcinho.13@hotmail.com>
2015-12-26 19:24:32 -0200

    Alteração github
    classe
    
    correçao xml

--- app/src/main/java/libelulati/tripctrl/MetodosPagamento/MetodosPagamentoEditActivity.java
+++ app/src/main/java/libelulati/tripctrl/MetodosPagamento/MetodosPagamentoEditActivity.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,158 +18,163 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import libelulati.tripctrl.Funcoes.Validar;
 import libelulati.tripctrl.Inicio.InicioActivity;
 import libelulati.tripctrl.R;
 import libelulati.tripctrl.Strings.MensagensUsuario;
+import libelulati.tripctrl.Viagens.ViagensDAO;
 
 
 public class MetodosPagamentoEditActivity extends AppCompatActivity {
 
-    private Spinner spinnerUsuario , spinnerTipoPagamento , spinnerViagem;
-    private List<String> listaUsuario, listaTipoPagamento, listaViagem;
+    private Spinner spinnerTipoPagamento, spinnerViagem;
+    private List<String>  listaTipoPagamento, listaViagem;
     MetodosPagamentoDAO data;
-    private EditText ed_mp_usuario, ed_mp_viagem, ed_mp_tipoPagamento;
+    private EditText ed_mp_viagem, ed_mp_tipoPagamento;
+    int usuario = InicioActivity.getId_uslogado();
+    boolean validar, valido;
+    Context context;
+    EditText mp_detalhe, mp_valor, mp_vencimento;
+    private int id;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metodos_pagamento_edit);
 
-             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
-
-        this.ed_mp_usuario = (EditText) findViewById(R.id.ed_mp_usuario);
-        spinnerUsuario = (Spinner) findViewById(R.id.sp_mp_usuario);
-        listaUsuario = new ArrayList<String>();
-        data = new MetodosPagamentoDAO(this);
-        this.ed_mp_usuario.setText("");
-        listaUsuario = data.SpinerUsuario();
-
-        ArrayAdapter<String> adpterUsuario = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listaUsuario);
-        adpterUsuario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUsuario.setAdapter(adpterUsuario);
-
-        spinnerUsuario.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
-
-                Toast.makeText(arg0.getContext(), "selecionado: " + arg0.getItemAtPosition(arg2).toString(), Toast.LENGTH_SHORT).show();
-                ed_mp_usuario.setText(arg0.getItemAtPosition(arg2).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-        });
-
-        this.ed_mp_viagem = (EditText) findViewById(R.id.ed_mp_viagem);
-        spinnerUsuario = (Spinner) findViewById(R.id.sp_mp_viagem);
-        listaUsuario = new ArrayList<String>();
-        data = new MetodosPagamentoDAO(this);
-        this.ed_mp_viagem.setText("");
-        listaViagem = data.SpinerViagem();
-
-        ArrayAdapter<String> adpterViagem = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listaUsuario);
-        adpterUsuario.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUsuario.setAdapter(adpterViagem);
-
-        spinnerUsuario.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
-
-                Toast.makeText(arg0.getContext(), "selecionado: " + arg0.getItemAtPosition(arg2).toString(), Toast.LENGTH_SHORT).show();
-                ed_mp_usuario.setText(arg0.getItemAtPosition(arg2).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-        });
-
-
-        this.ed_mp_tipoPagamento = (EditText) findViewById(R.id.ed_mp_tipopagamento);
-        spinnerTipoPagamento = (Spinner) findViewById(R.id.sp_mp_tipopagamento);
-        listaTipoPagamento = new ArrayList<String>();
-        data = new MetodosPagamentoDAO(this);
-        this.ed_mp_usuario.setText("");
-        listaTipoPagamento = data.SpinerTipoPagamento();
-
-        ArrayAdapter<String> adpterTipoPagamento = new ArrayAdapter<String>(this, android.R.layout.activity_list_item, listaTipoPagamento);
-        adpterTipoPagamento.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTipoPagamento.setAdapter(adpterTipoPagamento);
-        spinnerTipoPagamento.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
-
-                Toast.makeText(arg0.getContext(), "selecionado: " + arg0.getItemAtPosition(arg2).toString(), Toast.LENGTH_SHORT).show();
-                ed_mp_tipoPagamento.setText(arg0.getItemAtPosition(arg2).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
 
 
-        final int usuario = InicioActivity.getId_uslogado();
-    final Spinner sp_idViagem = (Spinner) findViewById(R.id.sp_mp_viagem);
-    final Spinner sp_tipopagamento = (Spinner) findViewById(R.id.sp_mp_tipopagamento);
-    final EditText mp_detalhe = (EditText) findViewById(R.id.ed_mp_detalhe);
-    final EditText mp_valor = (EditText) findViewById(R.id.ed_mp_Valor);
-    final EditText mp_vencimento = (EditText) findViewById(R.id.ed_mp_vencimento);
-    final Button mp_bt_salvar = (Button) findViewById(R.id.bt_mp_salvar);
+            spinnerViagem = (Spinner) findViewById(R.id.sp_mp_viagem);
+            spinnerTipoPagamento = (Spinner) findViewById(R.id.sp_mp_tipopagamento);
+            mp_detalhe = (EditText) findViewById(R.id.ed_mp_detalhe);
+            mp_valor = (EditText) findViewById(R.id.ed_mp_Valor);
+            mp_vencimento = (EditText) findViewById(R.id.ed_mp_vencimento);
+
+            spinnerViagem.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        valido = false;
+                        ed_mp_viagem.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    } else {
+                        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(spinnerViagem.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        verificaViagem();
+                    }
+                }
+            });
 
 
-        mp_bt_salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            final Context context = v.getContext();
-            MetodosPagamento mp = new MetodosPagamento();
-
-            mp.setUs_id(usuario);
-            mp.setDv_id(Integer.parseInt(sp_idViagem.getAdapter().toString()));
-            mp.setTp_id(Integer.parseInt(sp_tipopagamento.getAdapter().toString()));
-            mp.setMe_detalhes(mp_detalhe.getText().toString());
-            mp.setMe_valor(Integer.parseInt(mp_valor.getText().toString()));
-            mp.setMe_vencimento(mp_vencimento.getText().toString());
-
-                boolean sucesso = new MetodosPagamentoDAO(context).criar(mp);
-
-            if (sucesso) {
-                Toast.makeText(context, MensagensUsuario.getMETODOPAGAMENTO() + MensagensUsuario.getCriado_sucesso(), Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(context, MensagensUsuario.getCriado_sucesso() + MensagensUsuario.getMETODOPAGAMENTO(), Toast.LENGTH_LONG).show();
-            }
-
-            finish();
+            mp_valor.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        valido = false;
+                        mp_valor.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                    } else {
+                        ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(mp_valor.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        verificarvalor();
+                    }
+                }
+            });
         }
 
-    });
-}
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            getMenuInflater().inflate(R.menu.menu_global, menu);
+            return (true);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            switch (id){
+                case R.id.mn_gb_salvar:
+                    salvar();
+                case android.R.id.home:
+                    if(getActionBar() == null){
+                        onBackPressed();
+                    }
+                    else{
+                        NavUtils.navigateUpFromSameTask(this);
+                    }
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+        public void salvar(){
+            MetodosPagamento metodosPagamento = new MetodosPagamento();
+
+            metodosPagamento.setUs_id(usuario);
+            metodosPagamento.setDv_id((int) spinnerViagem.getAdapter().getItemId(id));
+            metodosPagamento.setTp_id((int) spinnerTipoPagamento.getAdapter().getItemId(id));
+            metodosPagamento.setMe_detalhes(mp_detalhe.getText().toString());
+            metodosPagamento.setMe_valor(Integer.parseInt(mp_valor.getText().toString()));
+            metodosPagamento.setMe_vencimento(mp_vencimento.getText().toString());
+
+            if(valido){
+                boolean sucesso = new MetodosPagamentoDAO(context).criar(metodosPagamento);
+
+                if (sucesso) {
+                    Toast.makeText(context, MensagensUsuario.getVIAGEM() + MensagensUsuario.getCriado_sucesso(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(context, MensagensUsuario.getErro_criar() + MensagensUsuario.getMETODOPAGAMENTO(), Toast.LENGTH_LONG).show();
+                }
+
+                finish();
+            }
+            else{
+                Toast.makeText(context, "Há campos inválidos. O registro não será gravado;", Toast.LENGTH_SHORT).show();
+            }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case android.R.id.home:
-                if(getActionBar() == null){
-                    onBackPressed();
+        }
+
+        public void verificaViagem(){
+            validar = Validar.ValidarNome(String.valueOf(spinnerViagem.getAdapter().getItem(id)));
+            if(!validar){
+                ed_mp_viagem.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
+                Toast.makeText(context, "Nome da viagem inválido", Toast.LENGTH_SHORT).show();
+                valido = false;
+            }
+            else{
+                ed_mp_viagem.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                valido = true;
+            }
+        }
+
+
+        public void verificarvalor(){
+            double valor = 0;
+            valor = Double.parseDouble(mp_valor.getText().toString());
+            if(valor == 0){
+                validar = Validar.ValidarValor(valor);
+                if(!validar){
+                    mp_valor.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
+                    Toast.makeText(context, "Valor inválido", Toast.LENGTH_SHORT).show();
+                    valido = false;
                 }
                 else{
-                    NavUtils.navigateUpFromSameTask(this);
+                    mp_valor.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+                    valido = true;
                 }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+            }
+            else{
+                mp_valor.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
+                Toast.makeText(context, "Valor inválido", Toast.LENGTH_SHORT).show();
+            }
         }
-    }
+
 }
