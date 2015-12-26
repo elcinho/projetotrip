4c2dba39e399fbf483aa0284d7000b271b2c054a
Marcela Melo <marcelaamelo1986@gmail.com>
2015-12-26 19:06:10 -0200

    alteração do crud Viagem

--- app/src/main/java/libelulati/tripctrl/Viagens/ViagensNovoActivity.java
+++ app/src/main/java/libelulati/tripctrl/Viagens/ViagensNovoActivity.java
@@ -230,26 +230,22 @@ public class ViagensNovoActivity extends AppCompatActivity {
         double valor = 0;
         if(vi_valortotal.length()>0){
             valor = Double.parseDouble(vi_valortotal.getText().toString());
+            if(valor != 0){
+                validar = Validar.ValidarValor(valor);
+                if(!validar){
+                    vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
+                    Toast.makeText(context, "Valor inválido", Toast.LENGTH_SHORT).show();//ALTERAR PARA STRINGS DO SISTEMA
+                    valido = false;
+                }
+                else{
+                    vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+                    valido = true;
+                }
+            }
         }
         else{
             vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
             Toast.makeText(context, "Valor não pode ser vazio", Toast.LENGTH_SHORT).show();//ALTERAR PARA STRINGS DO SISTEMA
-        }
-        if(valor != 0){
-            validar = Validar.ValidarValor(valor);
-            if(!validar){
-                vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
-                Toast.makeText(context, "Valor inválido", Toast.LENGTH_SHORT).show();//ALTERAR PARA STRINGS DO SISTEMA
-                valido = false;
-            }
-            else{
-                vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
-                valido = true;
-            }
-        }
-        else{
-            vi_valortotal.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.alert_icon,0);
-            Toast.makeText(context, "Valor inválido", Toast.LENGTH_SHORT).show();//ALTERAR PARA STRINGS DO SISTEMA
         }
     }
 
