3ec70d5e8c86697415a86deaebb0491318e57dfe
Marcela Melo <marcelaamelo1986@gmail.com>
2016-03-01 18:12:39 -0300

    Implementação Lista Configurações

--- app/src/main/java/libelulati/tripctrl/Configuracoes/ConfiguracoesListActivity.java
+++ app/src/main/java/libelulati/tripctrl/Configuracoes/ConfiguracoesListActivity.java
@@ -2,17 +2,14 @@ package libelulati.tripctrl.Configuracoes;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import libelulati.tripctrl.Inicio.InicioActivity;
 import libelulati.tripctrl.R;
@@ -26,25 +23,29 @@ public class ConfiguracoesListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes_list);
 
+        context = ConfiguracoesListActivity.this;
+
         LinearLayout linearLayout_itens = (LinearLayout) findViewById(R.id.li_co_lista);
         linearLayout_itens.removeAllViews();
 
-        ArrayList<String> co_itens = new ArrayList<>();
-        co_itens.add(context.getResources().getString(R.string.co_notificacoes));
-        co_itens.add(context.getResources().getString(R.string.co_perfil_usuario));
-        co_itens.add(context.getResources().getString(R.string.co_categorias));
-        co_itens.add(context.getResources().getString(R.string.co_tipos_pagamento));
-        co_itens.add(context.getResources().getString(R.string.co_sobre));
-        co_itens.add(context.getResources().getString(R.string.co_avalie));
-        co_itens.add(context.getResources().getString(R.string.co_termo_uso));
-        co_itens.add(context.getResources().getString(R.string.co_sair));
+        List<String> co_itens = new ArrayList<>();
+        if(co_itens.size() == 0){
+            co_itens.add(context.getResources().getString(R.string.co_notificacoes));
+            co_itens.add(context.getResources().getString(R.string.co_perfil_usuario));
+            co_itens.add(context.getResources().getString(R.string.co_categorias));
+            co_itens.add(context.getResources().getString(R.string.co_tipos_pagamento));
+            co_itens.add(context.getResources().getString(R.string.co_sobre));
+            co_itens.add(context.getResources().getString(R.string.co_avalie));
+            co_itens.add(context.getResources().getString(R.string.co_termo_uso));
+            co_itens.add(context.getResources().getString(R.string.co_sair));
+        }
 
         View viewItens = null;
         TextView tx_co_itens;
 
-        for (int i = 0; i <= co_itens.size(); i++){
+        for (int i = 0; i < co_itens.size(); i++){
             int item_id = i;
-            String item = co_itens.toString();
+            String item = co_itens.get(i);
 
             LayoutInflater inflater = getLayoutInflater();
             viewItens = inflater.inflate(R.layout.view_list_configuracoes, null);
@@ -53,6 +54,8 @@ public class ConfiguracoesListActivity extends AppCompatActivity {
             tx_co_itens.setText(item);
 
             viewItens.setTag(item_id);
+
+            linearLayout_itens.addView(viewItens);
         }
     }
 }
