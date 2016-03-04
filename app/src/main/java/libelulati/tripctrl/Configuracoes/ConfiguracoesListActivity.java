c6e345dcfc0f000efe82f97f1fafb76f88a5a049
Marcela Melo <marcelaamelo1986@gmail.com>
2016-03-04 12:35:14 -0300

    botão sair

--- app/src/main/java/libelulati/tripctrl/Configuracoes/ConfiguracoesListActivity.java
+++ app/src/main/java/libelulati/tripctrl/Configuracoes/ConfiguracoesListActivity.java
@@ -91,6 +91,10 @@ public class ConfiguracoesListActivity extends AppCompatActivity {
             case 7:
                 break;
             case 8:
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
                 break;
         }
     }
