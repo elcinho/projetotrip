821bfb05977f373e46f7e1ac9d03f6f047b52dd2
Marcela Melo <marcelaamelo1986@gmail.com>
2016-03-04 12:17:17 -0300

    Implementação de Categorias - finalizado

--- app/src/main/java/libelulati/tripctrl/Categorias/CategoriaListActivity.java
+++ app/src/main/java/libelulati/tripctrl/Categorias/CategoriaListActivity.java
@@ -1,15 +1,16 @@
 package libelulati.tripctrl.Categorias;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,9 +23,9 @@ import libelulati.tripctrl.R;
 
 public class CategoriaListActivity extends AppCompatActivity {
     int id_usuario = InicioActivity.getId_usuario();
-    int id_categoria;
     Context context;
-    EditText ed_ca_novo;
+    EditText ed_ca_novo, ed_di_editar;
+    TextView tx_ca_nome;
     ImageView ib_ca_novo;
 
     @Override
@@ -35,8 +36,12 @@ public class CategoriaListActivity extends AppCompatActivity {
         context = CategoriaListActivity.this;
 
         ed_ca_novo = (EditText) findViewById(R.id.ed_ca_novo);
-        ib_ca_novo = (ImageView) findViewById(R.id.iv_ca_novo);
 
+        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(ed_ca_novo.getWindowToken(), 0);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        ib_ca_novo = (ImageView) findViewById(R.id.iv_ca_novo);
         ib_ca_novo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +69,11 @@ public class CategoriaListActivity extends AppCompatActivity {
                 Toast.makeText(context, context.getResources().getString(R.string.sucesso_criar_categoria), Toast.LENGTH_LONG).show();
                 ed_ca_novo.setText("");
                 ed_ca_novo.clearFocus();
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(ed_ca_novo.getWindowToken(), 0);
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
                 Listar();
             }
             else{
@@ -75,8 +85,23 @@ public class CategoriaListActivity extends AppCompatActivity {
         }
     }
 
-    public void Atualizar(){
+    public void Atualizar(int id){
+        Categoria categoria = new Categoria();
+        categoria.setUs_id(id_usuario);
+        categoria.setCa_nome(ed_di_editar.getText().toString());
 
+        if(IsValido()){
+            boolean sucesso = new Categorias_DAO(context).atualizar(categoria, id);
+            if(sucesso){
+                Listar();
+            }
+            else {
+                Toast.makeText(context, context.getResources().getString(R.string.erro_alterar_categoria), Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(context, context.getResources().getString(R.string.erro_validar_campos), Toast.LENGTH_LONG).show();
+        }
     }
 
     public void Listar(){
@@ -85,19 +110,20 @@ public class CategoriaListActivity extends AppCompatActivity {
 
         List<Categoria> categorias = new Categorias_DAO(context).listar(id_usuario);
         View viewItens = null;
-        TextView tx_ca_nome;
 
         if(categorias.size() > 0){
             for(final Categoria categoria : categorias){
                 final int id_ca = categoria.getCa_id();
                 int id_us = categoria.getUs_id();
-                String ca_nome = categoria.getCa_nome();
+                final String ca_nome = categoria.getCa_nome();
 
                 LayoutInflater inflater = getLayoutInflater();
                 viewItens = inflater.inflate(R.layout.view_list_textview, null);
 
                 tx_ca_nome = (TextView)viewItens.findViewById(R.id.tx_vw_textview);
+
                 tx_ca_nome.setText(ca_nome);
+                tx_ca_nome.requestFocus();
 
                 viewItens.setTag(id_ca);
 
@@ -105,14 +131,14 @@ public class CategoriaListActivity extends AppCompatActivity {
                     viewItens.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Editar();
+                            Editar(id_ca, ca_nome);
                         }
                     });
 
                     viewItens.setOnLongClickListener(new View.OnLongClickListener() {
                         @Override
                         public boolean onLongClick(View v) {
-                            Deletar();
+                            Deletar(id_ca);
                             return false;
                         }
                     });
@@ -123,12 +149,67 @@ public class CategoriaListActivity extends AppCompatActivity {
         }
     }
 
-    public void Deletar(){
-        Toast.makeText(context, "teste long Click", Toast.LENGTH_LONG).show();
+    public void Deletar(int id){
+        final int del_id = id;
+
+        AlertDialog confirme;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getResources().getString(R.string.opcao_confirmar));
+        builder.setMessage(context.getResources().getString(R.string.excluir_registro));
+
+        builder.setPositiveButton(context.getResources().getString(R.string.opcao_ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Categorias_DAO categorias_dao = new Categorias_DAO(context);
+                boolean sucesso = categorias_dao.deletar(del_id);
+                if (sucesso) {
+                    Toast.makeText(context, context.getResources().getString(R.string.sucesso_excluir_categoria), Toast.LENGTH_LONG).show();
+                    Listar();
+                } else {
+                    Toast.makeText(context, context.getResources().getString(R.string.erro_excluir_categoria), Toast.LENGTH_LONG).show();
+                }
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(context.getResources().getString(R.string.opcao_cancelar), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        confirme = builder.create();
+        confirme.show();
     }
 
-    public void Editar(){
-        Toast.makeText(context, "teste short Click", Toast.LENGTH_LONG).show();
+    public void Editar(final int id, String nome) {
+        AlertDialog editardialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(context.getResources().getString(R.string.editar) + " " + nome);
+        LayoutInflater inflater = getLayoutInflater();
+        View layoutview = inflater.inflate(R.layout.dialog_editar_edittext, null);
+        builder.setView(layoutview);
+        ed_di_editar = (EditText)layoutview.findViewById(R.id.ed_di_editar);
+        ed_di_editar.setText(nome);
+
+        builder.setPositiveButton(context.getResources().getString(R.string.opcao_ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Atualizar(id);
+            }
+        });
+
+        builder.setNegativeButton(context.getResources().getString(R.string.opcao_cancelar), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        editardialog = builder.create();
+        editardialog.show();
     }
 
     public boolean IsValido(){
