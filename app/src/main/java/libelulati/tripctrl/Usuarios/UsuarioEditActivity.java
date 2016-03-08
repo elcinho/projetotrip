b28e6618dc33eac79bc4300c19941d58bd9598ff
Marcela Melo <marcelaamelo1986@gmail.com>
2016-03-08 12:36:14 -0300

    implementação usuario - incompleto

--- app/src/main/java/libelulati/tripctrl/Usuarios/UsuarioEditActivity.java
+++ app/src/main/java/libelulati/tripctrl/Usuarios/UsuarioEditActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import libelulati.tripctrl.Dados.Nomes;
 import libelulati.tripctrl.Funcoes.DatePicker;
@@ -45,6 +50,7 @@ public class UsuarioEditActivity extends AppCompatActivity {
         Bundle bundle = it_us_novo.getExtras();
 
         novo = bundle.getInt("novo");
+        id_usuario = bundle.getInt(Nomes.getID());
         ed_us_nome.setText(bundle.getString(Nomes.getUsNome()));
         ed_us_email.setText(bundle.getString(Nomes.getUsEmail()));
         ed_us_dtnasc.setText(bundle.getString(Nomes.getUsDtnasc()));
@@ -71,7 +77,37 @@ public class UsuarioEditActivity extends AppCompatActivity {
     }
 
     public void Salvar(){
+        final Usuario usuario = new Usuario();
 
+        usuario.setUs_nome(ed_us_nome.getText().toString());
+        usuario.setUs_email(ed_us_email.getText().toString());
+        usuario.setUs_dtnasc(ed_us_dtnasc.getText().toString());
+        usuario.setUs_senha(ed_us_senha.getText().toString());
+
+        cb_us_semsenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    usuario.setUs_semsenha(1);
+                } else {
+                    usuario.setUs_semsenha(0);
+                }
+            }
+        });
+
+        if(IsValido()){
+            boolean sucesso = new Usuario_DAO(context).criar(usuario);
+            if(sucesso){
+                Toast.makeText(context, context.getResources().getString(R.string.sucesso_criar_usuario), Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(context, context.getResources().getString(R.string.erro_criar_usuario), Toast.LENGTH_LONG).show();
+            }
+            finish();
+        }
+        else{
+            Toast.makeText(context,context.getResources().getString(R.string.erro_validar_campos),Toast.LENGTH_LONG).show();
+        }
     }
 
     public void UsuarioNovo(){
@@ -89,7 +125,7 @@ public class UsuarioEditActivity extends AppCompatActivity {
         ed_us_dtnasc.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus){
+                if (hasFocus) {
                     ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
                             .hideSoftInputFromWindow(ed_us_dtnasc.getWindowToken(),
                                     InputMethodManager.HIDE_NOT_ALWAYS);
@@ -139,6 +175,95 @@ public class UsuarioEditActivity extends AppCompatActivity {
 
         ed_us_nome.requestFocus();
     }
+
+    public void Atualizar(){
+        final Usuario usuario = new Usuario();
+
+        usuario.setUs_nome(ed_us_nome.getText().toString());
+        usuario.setUs_email(ed_us_email.getText().toString());
+        usuario.setUs_dtnasc(ed_us_dtnasc.getText().toString());
+
+        cb_us_semsenha.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    usuario.setUs_semsenha(1);
+                } else {
+                    usuario.setUs_semsenha(0);
+                }
+            }
+        });
+
+        if(IsValido()){
+            boolean sucesso = new Usuario_DAO(context).atualizar(usuario, id_usuario);
+            if(sucesso){
+                Toast.makeText(context, context.getResources().getString(R.string.sucesso_alterar_usuario), Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(context, context.getResources().getString(R.string.erro_alterar_usuario), Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(context, context.getResources().getString(R.string.erro_validar_campos), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean IsValido(){
+        return true;
+    }
+
+    public void AlterarSenha(){
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        switch (vis_menu) {
+            case 1:
+                getMenuInflater().inflate(R.menu.salvar_menu, menu);
+                break;
+            case 2:
+                getMenuInflater().inflate(R.menu.editar_menu, menu);
+                break;
+            case 3:
+                getMenuInflater().inflate(R.menu.usuario_menu, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                if(getActionBar() == null){
+                    onBackPressed();
+                }
+                else {
+                    NavUtils.navigateUpFromSameTask(this);
+                }
+                break;
+            case R.id.mn_gb_salvar:
+                Salvar();
+                break;
+            case R.id.mn_editar:
+                UsuarioEditar();
+                break;
+            case R.id.mn_us_salvar:
+                Atualizar();
+                break;
+            case R.id.mn_us_alterarsenha:
+                AlterarSenha();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+
 
 
 
